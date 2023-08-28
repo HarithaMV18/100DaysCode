ec8e9a9dac1725ee8977954b65837d7b2744bf76
Haritha <136883345+HarithaMV18@users.noreply.github.com>
2023-08-28 22:52:32 +0530

    Add files via upload

--- Day16-Calculator/index.js
+++ Day16-Calculator/index.js
@@ -2,44 +2,54 @@ let buttons = document.querySelectorAll(".btns");
 let del = document.querySelectorAll(".btns-del");
 let result = document.getElementById("content");
 let nums = "";
+let count = 0;
 
 function equals(nums) {
-    try{(eval(nums))
-  if (eval(nums)) {
-    nums=eval(nums)
-    result.innerHTML = nums;
+  try {
+    eval(nums);
+    if (eval(nums)) {
+      nums = eval(nums);
+      result.innerHTML = nums;
+    } else {
+      nums = "Error";
+      result.innerHTML = "Error";
+    }
+  } catch (e) {
+    nums = "Error";
 
-  } else {
-    nums="Error"
     result.innerHTML = "Error";
-  
   }
-}
-catch(e){
-    nums="Error"
-  
-    result.innerHTML = "Error";
-}
-return nums
+  return JSON.stringify(nums);
 }
 function clearFunc(value) {
   nums = "";
   result.innerHTML = "";
+  count = 0;
+}
+function removeFunc(value) {
+  nums = nums.slice(0, -1);
+  result.innerHTML = nums;
 }
 for (let i = 0; i < buttons.length; i++) {
- 
   buttons[i].addEventListener("click", (event) => {
-    if(nums==="Error"){
-        clearFunc(true)
+    if (nums === "Error") {
+      clearFunc(true);
     }
+    else if(count===1 && event.target.innerHTML.match(/[0-9]/g)!==null){
+        nums = "";
+        result.innerHTML = "";
+        count = 0;
 
-
- 
+    }
+    else{
+        count=0;
+    }
     nums += event.target.innerHTML;
     result.innerHTML = nums;
     if (event.target.innerHTML === "=") {
-        nums=nums.slice(0, -1)
-      nums=equals(nums);
+      nums = nums.slice(0, -1);
+      nums = equals(nums);
+      count=1;
     }
   });
 }
