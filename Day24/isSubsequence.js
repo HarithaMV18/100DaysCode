9aaae314274d1688efa60183509ec5cca6d7b054
Haritha <136883345+HarithaMV18@users.noreply.github.com>
2023-09-19 09:12:44 +0530

    Update isSubsequence.js

--- Day24/isSubsequence.js
+++ Day24/isSubsequence.js
@@ -25,3 +25,21 @@ var isSubsequence = function (s, t) {
   }
 };
 console.log(isSubsequence("b", "abc"));
+
+
+
+//another solution
+var isSubsequence = function (s, t) {
+  let currentIndex = -1;
+  for (let i = 0; i < s.length; i++) {
+    const target = t.indexOf(s[i], currentIndex + 1);
+    console.log(t.indexOf(s[i], currentIndex + 1))
+    if (target > currentIndex) {
+      currentIndex = target;
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
+console.log(isSubsequence("aaa", "abacade"));
