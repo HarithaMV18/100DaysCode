a0ba8f7571416f0ebcbb6646b418dca713ea3069
Haritha <136883345+HarithaMV18@users.noreply.github.com>
2023-09-19 08:57:47 +0530

    Update findTheDifference.js

--- Day23/findTheDifference.js
+++ Day23/findTheDifference.js
@@ -1,12 +1,12 @@
-// var findTheDifference = function(s, t) {
-// let sNew=s.split("").sort().join("");
-// let tNew=t.split("").sort().join("");
-// for(let i=0;i<tNew.length;i++){
-//   if(tNew[i]!==sNew[i]){
-//     return tNew[i]
-//   }
+ var findTheDifference = function(s, t) {
+ let sNew=s.split("").sort().join("");
+ let tNew=t.split("").sort().join("");
+for(let i=0;i<tNew.length;i++){
+  if(tNew[i]!==sNew[i]){
+     return tNew[i]
+  }
 
-// }
+ }
 
-// };
-// console.log(findTheDifference("abc","cbba"))
+ };
+ console.log(findTheDifference("abc","cbba"))
