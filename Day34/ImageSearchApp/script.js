0d8cbd8637325d4248eda268c86d3368f0951c54
Haritha <136883345+HarithaMV18@users.noreply.github.com>
2023-10-10 19:10:41 +0530

    Add files via upload

--- Day34/ImageSearchApp/script.js
+++ Day34/ImageSearchApp/script.js
@@ -8,29 +8,34 @@ let dataArr=[];
 let page=1;
 
 const getImages=async(inputImgValue,page)=>{
+    
     try {
        
         const url=`https://api.unsplash.com/search/photos?page=${page}&query=${inputImgValue}&client_id=${apiKey}`;
         const getImagesData=await fetch(url).then((response)=>response.json())
-    
-        if(page===1){
-            dataArr=getImagesData.results;
-        }
-        else{
-            dataArr=[...dataArr,...getImagesData.results]
-        }
-      
-     
-       main_img_container.innerHTML=dataArr.map((item)=>{
-       
-       return `<div class="images">
-       <a href=${item.links.html} target="_blank"><img src=${item.urls.small} alt=${item.alt_description
-       }></a>   
-          <p id="img-description">${item.alt_description}</p>
-      </div>`
+if(getImagesData.results!=0){
+    main_img_container.style.justifyContent="space-between"
+    if(page===1){
+        dataArr=getImagesData.results;
+    }
+    else{
+        dataArr=[...dataArr,...getImagesData.results]
+    }
+    main_img_container.innerHTML=dataArr.map((item)=>{
+   return `<div class="images">
+   <a href=${item.links.html} target="_blank"><img src=${item.urls.regular} alt=${item.alt_description}></a>   
+      <p id="img-description">${item.alt_description}</p>
+  </div>`
 
-       }).join("")
-       load_more_btn.style.display="block";
+   }).join("")
+   load_more_btn.style.display="block";
+}
+else{
+    load_more_btn.style.display="none";  
+    main_img_container.style.justifyContent="center"
+    main_img_container.innerHTML="No images found"
+}
+     
         
     } catch (error) {
         console.log(error)
