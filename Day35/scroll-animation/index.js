d56fd72397fd4a9f4646ce51ed1a522554483d25
Haritha <136883345+HarithaMV18@users.noreply.github.com>
2023-10-12 12:12:37 +0530

    Update index.js

--- Day35/scroll-animation/index.js
+++ Day35/scroll-animation/index.js
@@ -1,6 +1,6 @@
 const content_box=document.querySelectorAll(".content-box");
 window.addEventListener("scroll",function(){
-    const triggerHeight=Math.round(window.innerHeight/5*4)
+    const triggerHeight=Math.ceil(window.innerHeight/5*4)
     //The innerHeight property returns the height of a window's content area.
     content_box.forEach((item)=>{
         const item_top=item.getBoundingClientRect().top;
