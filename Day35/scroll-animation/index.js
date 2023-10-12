482a1caa49d7bef4719a170e7bb6701b24bad773
Haritha <136883345+HarithaMV18@users.noreply.github.com>
2023-10-12 11:48:22 +0530

    Add files via upload

---
+++ Day35/scroll-animation/index.js
@@ -0,0 +1,17 @@
+const content_box=document.querySelectorAll(".content-box");
+window.addEventListener("scroll",function(){
+    const triggerHeight=(window.innerHeight/5*4)
+    //The innerHeight property returns the height of a window's content area.
+    content_box.forEach((item)=>{
+        const item_top=item.getBoundingClientRect().top;
+        if(item_top<triggerHeight){
+            item.classList.add("show")
+        }
+        else{
+            item.classList.remove("show")
+        }
+    })
+
+
+    
+})
