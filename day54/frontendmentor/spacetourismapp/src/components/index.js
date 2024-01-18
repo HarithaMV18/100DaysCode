e5a0f750fb678f6527f9fcfcb806c97e7e30f697
Haritha <136883345+HarithaMV18@users.noreply.github.com>
2024-01-18 23:53:22 +0530

    Add files via upload

--- day54/frontendmentor/spacetourismapp/src/components/index.js
+++ day54/frontendmentor/spacetourismapp/src/components/index.js
@@ -1,8 +1,6 @@
 export { default as Home } from "./home/Home";
 export { default as Destination } from "./destination/Destination";
-// export { default as Moon } from "./destination/Moon";
-// export { default as Mars } from "./destination/Mars";
-export { default as Crew } from "./Crew";
+export { default as Crew } from "./crew/Crew";
 export { default as Technology } from "./Technology";
 export { default as Navbar } from "./navbar/Navbar";
 export { default as logo } from "../starter-code/assets/shared/logo.svg";
